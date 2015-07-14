814be270accfb752d481bc6495c10d8a9af4f13d
Harrsh Ankola <harsh.ankola@gmail.com>
2015-07-14 15:21:25 +0530

    Version 1.2.0.2T
    + Setting up for unit testing of components.

--- app/src/androidTest/java/org/skdrdpindia/cashcollectionapp/ApplicationTest.java
+++ app/src/androidTest/java/org/skdrdpindia/cashcollectionapp/ApplicationTest.java
@@ -10,4 +10,26 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    /**
+     * This will do the work to instantiate the Application under test.  After this, your test
+     * code must also start and stop the Application.
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /**
+     * Shuts down the Application under test.  Also makes sure all resources are cleaned up and
+     * garbage collected before moving on to the next
+     * test.  Subclasses that override this method should make sure they call super.tearDown()
+     * at the end of the overriding method.
+     *
+     * @throws Exception
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
